3d4ee7cd89e0c8c6902c0b2463bdc9f5a60fe847
Andrey Polyakov <polyakovv.andrey.01@gmail.com>
2023-01-02 15:03:28 +0200

    models Task and Tag were created

--- to_do_list/models.py
+++ to_do_list/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Tags(models.Model):
+    tag = models.CharField(max_length=255)
+
+
+class Task(models.Model):
+    content = models.TextField()
+    time_creation = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(blank=True, null=True)
+    completeness = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tags)
+
+    class Meta:
+        ordering = ["-completeness", "time_creation"]
