55120b77c03522f787229cf22b2e60f688c1ef79
Andrey Polyakov <polyakovv.andrey.01@gmail.com>
2023-01-02 18:22:26 +0200

    created list views for task and tags

--- to_do_list/views.py
+++ to_do_list/views.py
@@ -1,3 +1,16 @@
+from django.views import generic
 from django.shortcuts import render
 
-# Create your views here.
+from to_do_list.models import Task, Tags
+
+
+class ToDoList(generic.ListView):
+    model = Task
+    template_name = "to_do_list/home.html"
+    paginate_by = 5
+
+
+class Tag(generic.ListView):
+    model = Tags
+    template_name = "to_do_list/tags.html"
+    paginate_by = 5
