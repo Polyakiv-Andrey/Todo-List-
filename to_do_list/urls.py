6b37456b19d1b70e0f776e3a1b83250e46a3480a
Andrey Polyakov <polyakovv.andrey.01@gmail.com>
2023-01-02 22:21:59 +0200

    Created ToDoListView, CreateTaskView, UpdateTaskVie, DeleteTaskView and change_task_status

--- to_do_list/urls.py
+++ to_do_list/urls.py
@@ -3,8 +3,13 @@ from django.urls import path
 from to_do_list.views import *
 
 urlpatterns = [
-    path("", ToDoList.as_view(), name="ToDoList"),
-    path("tags/", Tag.as_view(), name="Tags")
+    path("", ToDoListView.as_view(), name="ToDoList"),
+    path("creat-task/", CreateTaskView.as_view(), name="create-task"),
+    path("update-task/<int:pk>/", UpdateTaskView.as_view(), name="update-task"),
+    path("delete-task/<int:pk>/", DeleteTaskView.as_view(), name="delete-task"),
+    path("task-status/<int:pk>/", change_task_status, name="change-task-status"),
+    path("tags/", TagListView.as_view(), name="Tags"),
+
 ]
 
 app_name = "to_do_list"
